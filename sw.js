d98288b3a66e94fddcb825dc102e6e7d7dacb077
elias1415 <elias.11179@gmail.com>
2025-08-28 16:49:50 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME='diet-sa-final3-v1';
+const CACHE_NAME='diet-sa-final3-v2';
 const ASSETS=['/','/index.html','/css/styles.css','/js/app.js','/js/meals.js','/js/tracker.js','/assets/cover.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))) });
